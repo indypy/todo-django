998872b68ea943517c1dbbf4dde4c78c18d59dd8
fezick <jason@directemployersfoundation.org>
2013-02-21 11:26:55 -0500

    Add ajax, views, urls, and tempaltes for retrieving jobs, searching jobs, searching tags.

--- direct_todo/direct_todo/urls.py
+++ direct_todo/direct_todo/urls.py
@@ -14,16 +14,28 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^$', 'direct_todo.views.home'),
-    url(r'^login/$', 
+    url(r'login/', 
         'django.contrib.auth.views.login', 
-        {
-            'template_name': 'index.html'
-            }
+        {'template_name': 'index.html'}
         ),
+    url(r'logout/', 
+        'django.contrib.auth.views.logout', 
+        {'template_name': 'index.html'}
+        ),    
+    url(r'^profile/tags/$', 
+        'direct_todo.views.filter_by_tag'),
+    
+    url(r'^profile/tags/(?P<tag>\w+)/$', 
+        'direct_todo.views.filter_by_tag'),
+    
     url(r'^profile/add-task/$', 
         'direct_todo.views.add_task'),
+    url(r'^profile/get-task/(?P<task_id>\w+)/$', 
+        'direct_todo.views.get_task'),
     url(r'^profile/delete-task/(?P<task_id>\w+)/$', 
-        'direct_todo.views.delete_task'),    
+        'direct_todo.views.delete_task'),
+    
+    
     url(r'^profile/$', 'direct_todo.views.profile'),    
     url(r'^admin/', include(admin.site.urls)),    
 )
--- direct_todo/direct_todo/views.py
+++ direct_todo/direct_todo/views.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response
 from direct_todo.models import *
+from taggit.models import *
 from django.contrib.auth import authenticate, login
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
@@ -13,19 +14,29 @@ def home(request):
 @login_required
 def profile(request):
     user=request.user
-    tasks = Task.objects.filter(user=user)
+    search = request.GET.get("search")
+        
+    if search:
+        tasks = Task.objects.filter(user=user).filter(title__contains=search)
+    else:
+        tasks = Task.objects.filter(user=user)
     ctx= {
         "user":user,
-        "tasks":tasks
+        "tasks":tasks,
+        "search":search
     }
     return render_to_response("profile.html",ctx,
                               context_instance=RequestContext(request))
-    
+
+
+@login_required
 def delete_task(request,task_id):
     task = Task.objects.get(id=task_id)
     task.delete()
     return HttpResponse("true")
 
+
+@login_required
 def add_task(request):
     task_title = request.GET.get('title')
     task_date = datetime.strptime(request.GET.get('date'),"%m/%d/%Y")
@@ -40,5 +51,29 @@ def add_task(request):
     #task.delete()
     return HttpResponse(new_task.id)
 
-#def get_task(request,task_id):
-#    document.get
+
+@login_required
+def get_task(request,task_id):
+    task = Task.objects.get(id=task_id)
+    ctx = {"task":task}
+    return render_to_response("single_task.html",ctx,
+                              context_instance=RequestContext(request))
+
+
+@login_required
+def filter_by_tag(request,tag=""):
+    user=request.user
+    ctx= {
+        "user":user,
+    }
+    template = "view_tags.html"
+    if tag:
+        tasks = Task.objects.filter(tags__name__in=[tag])
+        ctx["tasks"] = tasks
+        template = "profile.html"
+    else:
+        tags = Tag.objects.order_by('name')
+        ctx["tags"] = tags
+    
+    return render_to_response(template,ctx,
+                              context_instance=RequestContext(request))
